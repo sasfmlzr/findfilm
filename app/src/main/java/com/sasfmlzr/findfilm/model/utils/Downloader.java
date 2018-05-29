37e2333d815e4671fb98ce0f93fb9143a7114087
sasfmlzr <sasfmlzr@yandex.ru>
2018-05-29 17:01:11 +0300

    Add downloader image

---
+++ app/src/main/java/com/sasfmlzr/findfilm/model/utils/Downloader.java
@@ -0,0 +1,25 @@
+package com.sasfmlzr.findfilm.model.utils;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Downloader {
+
+    public static Bitmap downloadImage(String urlImage) {
+        try {
+            URL url = new URL(urlImage);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
+    }
+}
