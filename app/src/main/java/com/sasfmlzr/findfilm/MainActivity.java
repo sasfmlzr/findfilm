d3ac914480d6eb4b8c12af96ccf2a0c319963ea7
sasfmlzr <sasfmlzr@yandex.ru>
2018-05-29 13:58:51 +0300

    Add empty fragment

--- app/src/main/java/com/sasfmlzr/findfilm/MainActivity.java
+++ app/src/main/java/com/sasfmlzr/findfilm/MainActivity.java
@@ -2,9 +2,13 @@ package com.sasfmlzr.findfilm;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.sasfmlzr.findfilm.model.fragment.DiscoverFilmFragment;
+import com.sasfmlzr.findfilm.model.request.CurrentMovieRequest;
 import com.sasfmlzr.findfilm.model.request.DiscoverMovieRequest;
+import com.sasfmlzr.findfilm.model.request.JsonParserRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new RetrieveFeedTask().execute();
+
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.containerForFragment, new DiscoverFilmFragment())
+                    .commit();
+        }
     }
 
     static class RetrieveFeedTask extends AsyncTask<Void, Void, Void> {
@@ -26,9 +37,13 @@ public class MainActivity extends AppCompatActivity {
             String json = "";
             JSONObject jsonObject;
             try {
+                JsonParserRequest jsonParserRequest = new JsonParserRequest();
                 json = request.discoverMovie();
                 jsonObject = new JSONObject(json);
-                DiscoverMovieRequest request1 = new DiscoverMovieRequest(jsonObject);
+                DiscoverMovieRequest request1 = jsonParserRequest.discoverMovieParce(jsonObject);
+                json = request.viewMovie(383498);
+                jsonObject = new JSONObject(json);
+                CurrentMovieRequest currentMovieRequest = jsonParserRequest.currentMovieParce(jsonObject);
                 System.out.println();
             } catch (JSONException e) {
                 e.printStackTrace();
---
+++ app/src/main/java/com/sasfmlzr/findfilm/model/fragment/DiscoverFilmFragment.java
@@ -0,0 +1,23 @@
+package com.sasfmlzr.findfilm.model.fragment;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sasfmlzr.findfilm.R;
+
+public class DiscoverFilmFragment extends android.support.v4.app.Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.discover_fragment, container, false);
+    }
+}
