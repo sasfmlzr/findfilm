d864a29833c809081c4fa86ed89bb66404b28cf8
sasfmlzr <sasfmlzr@yandex.ru>
2018-05-29 17:00:53 +0300

    Add url for download

--- app/src/main/java/com/sasfmlzr/findfilm/model/SystemSettings.java
+++ app/src/main/java/com/sasfmlzr/findfilm/model/SystemSettings.java
@@ -1,4 +1,6 @@
 package com.sasfmlzr.findfilm.model;
 public class SystemSettings {
     public static final String API_KEY = "95b6fb97b8ac374f68020711a8b9ceec";
+    public static final String URL_IMAGE_92PX = "https://image.tmdb.org/t/p/w92";
+    public static final String URL_IMAGE_500PX = "https://image.tmdb.org/t/p/w500";
 }
