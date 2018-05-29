0affe9cd2b2a9c73a8e8f865f5010f46a4baafc7
sasfmlzr <sasfmlzr@yandex.ru>
2018-05-29 14:58:32 +0300

    Fill recycler view, which contain films

--- app/src/main/java/com/sasfmlzr/findfilm/model/adapter/DiscoverRecyclerAdapter.java
+++ app/src/main/java/com/sasfmlzr/findfilm/model/adapter/DiscoverRecyclerAdapter.java
@@ -2,32 +2,54 @@ package com.sasfmlzr.findfilm.model.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sasfmlzr.findfilm.R;
+import com.sasfmlzr.findfilm.model.request.DiscoverMovieRequest;
+
+import java.util.List;
 
 public class DiscoverRecyclerAdapter extends RecyclerView.Adapter<DiscoverRecyclerAdapter.ViewHolder> {
+    List<DiscoverMovieRequest.ResultsField> filmList;
 
+    public DiscoverRecyclerAdapter(List<DiscoverMovieRequest.ResultsField> filmList) {
+        this.filmList = filmList;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.discover_film_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        DiscoverMovieRequest.ResultsField currentFilm = filmList.get(position);
+        holder.nameFilm.setText(currentFilm.getTitle());
+        holder.descriptionFilm.setText(currentFilm.getOverview());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return filmList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final TextView nameFilm;
+        final TextView descriptionFilm;
+        final ImageView imageFilmView;
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
+            nameFilm = itemView.findViewById(R.id.nameFilm);
+            descriptionFilm = itemView.findViewById(R.id.descriptionFilm);
+            imageFilmView = itemView.findViewById(R.id.previewFilmImageView);
         }
 
         @Override
