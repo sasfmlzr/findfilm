513552c324dd1c4c6e0f3c98b8ee2a497b9e7b54
sasfmlzr <sasfmlzr@yandex.ru>
2018-05-29 13:59:16 +0300

    Add empty RecyclerView

---
+++ app/src/main/java/com/sasfmlzr/findfilm/model/adapter/DiscoverRecyclerAdapter.java
@@ -0,0 +1,38 @@
+package com.sasfmlzr.findfilm.model.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DiscoverRecyclerAdapter extends RecyclerView.Adapter<DiscoverRecyclerAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
