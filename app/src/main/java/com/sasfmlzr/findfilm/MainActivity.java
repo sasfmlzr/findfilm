5dfb250e73315637a4f31c9a343a56e2cb70aca6
sasfmlzr <sasfmlzr@yandex.ru>
2018-06-04 18:07:25 +0300

    Add empty search item for activity

--- app/src/main/java/com/sasfmlzr/findfilm/MainActivity.java
+++ app/src/main/java/com/sasfmlzr/findfilm/MainActivity.java
@@ -1,14 +1,20 @@
 package com.sasfmlzr.findfilm;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 import com.sasfmlzr.findfilm.fragment.CurrentFilmFragment;
 import com.sasfmlzr.findfilm.fragment.DiscoverFilmFragment;
 import com.sasfmlzr.findfilm.fragment.DiscoverFilmFragment.OnFilmSelectedListener;
 
 public class MainActivity extends AppCompatActivity implements OnFilmSelectedListener {
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,21 @@ public class MainActivity extends AppCompatActivity implements OnFilmSelectedLis
                     .add(R.id.containerForFragment, new DiscoverFilmFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
     }
 
     @Override
