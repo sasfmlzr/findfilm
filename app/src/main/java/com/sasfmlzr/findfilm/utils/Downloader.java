acb5e19bb01ba7711a038a4decfb8d16c5f4dc79
sasfmlzr <sasfmlzr@yandex.ru>
2018-06-13 16:43:48 +0300

    Replace HttpUrlConnection to OkHttp3

--- app/src/main/java/com/sasfmlzr/findfilm/utils/Downloader.java
+++ app/src/main/java/com/sasfmlzr/findfilm/utils/Downloader.java
@@ -3,24 +3,22 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Downloader {
     private static final String TAG = "DownloadImage";
 
     public static Bitmap downloadImage(String urlImage) {
+        OkHttpClient client = new OkHttpClient();
+        Request.Builder builder = new Request.Builder();
+        builder.url(urlImage);
+        Request requestMovie = builder.build();
         try {
-            URL url = new URL(urlImage);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            return BitmapFactory.decodeStream(input);
-        } catch (IOException e) {
-            // Log exception
+            Response response = client.newCall(requestMovie).execute();
+            return BitmapFactory.decodeStream(response.body().byteStream());
+        } catch (Exception e) {
             Log.d(TAG, "error download " + urlImage);
             return null;
         }
