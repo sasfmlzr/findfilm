b64d4cf591911597039bf8a6b52b7585531dbadf
sasfmlzr <sasfmlzr@yandex.ru>
2018-09-17 17:43:23 +0300

    Add mvvm for current film.

--- app/src/main/java/com/sasfmlzr/findfilm/fragment/currentfilm/CurrentFilmFragment.java
+++ app/src/main/java/com/sasfmlzr/findfilm/fragment/currentfilm/CurrentFilmFragment.java
@@ -1,136 +1,55 @@
 package com.sasfmlzr.findfilm.fragment.currentfilm;
 
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.button.MaterialButton;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sasfmlzr.findfilm.R;
-import com.sasfmlzr.findfilm.model.RetrofitSingleton;
-import com.sasfmlzr.findfilm.request.CurrentMovieRequest;
-import com.sasfmlzr.findfilm.request.FindFilmApi;
+import com.sasfmlzr.findfilm.databinding.CurrentFilmFragmentBinding;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
 
+import java.io.File;
 import java.util.Objects;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import static com.sasfmlzr.findfilm.model.SystemSettings.API_KEY;
-import static com.sasfmlzr.findfilm.model.SystemSettings.LANGUAGE;
-import static com.sasfmlzr.findfilm.model.SystemSettings.URL_IMAGE_500PX;
-
-public class CurrentFilmFragment extends Fragment {
+public class CurrentFilmFragment extends Fragment{
+    public static final String ARGUMENT_FILM_ID = "idFilm";
     private int idFilm;
-    private Unbinder unbinder;
-
-    @BindView(R.id.button_play_video)
-    ImageButton buttonPlayVideo;
-    @BindView(R.id.linear_layout_parent)
-    LinearLayout linearLayout;
-    @BindView(R.id.app_bar_layout)
-    AppBarLayout appBarLayout;
-    @BindView(R.id.coordinator_layout)
-    CoordinatorLayout coordinatorLayout;
-    @BindView(R.id.current_film_image_view)
-    ImageView posterFilm;
-    @BindView(R.id.description_current_film)
-    TextView description;
-    @BindView(R.id.vote_average)
-    TextView voteAverage;
-    @BindView(R.id.release_date)
-    TextView releaseDate;
-    @BindView(R.id.progressBarLoaderCurrentFilm)
-    ProgressBar progressLoaderImage;
-    @BindView(R.id.current_film_toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.collapsing_toolbar)
-    CollapsingToolbarLayout collapsingToolbarLayout;
-    @BindView(R.id.button_buy_tickets)
-    MaterialButton buttonBuyTickets;
+    private CurrentFilmViewModel viewModel;
+    private CurrentFilmFragmentBinding viewDataBinding;
 
     public static CurrentFilmFragment newInstance(int idFilm) {
         Bundle args = new Bundle();
         CurrentFilmFragment fragment = new CurrentFilmFragment();
-        args.putInt("idFilm", idFilm);
+        args.putInt(ARGUMENT_FILM_ID, idFilm);
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            idFilm = getArguments().getInt("idFilm");
-        }
-    }
-
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.current_film_fragment, container, false);
-        setHasOptionsMenu(true);
-        unbinder = ButterKnife.bind(this, view);
-        AppCompatActivity activity = (AppCompatActivity) getActivity();
 
-        if (activity != null) {
-            activity.setSupportActionBar(toolbar);
-            toolbar.setNavigationOnClickListener(item -> activity.onBackPressed());
-            Objects.requireNonNull(activity.getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        }
-        Drawable drawable = getResources().getDrawable(R.drawable.baseline_favorite_24);
-        int color = 0xFFAD563B;
-        drawable.setTint(color);
-        voteAverage.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
-        voteAverage.setCompoundDrawablePadding(20);
-        buttonBuyTickets.setOnClickListener(item -> Toast.makeText
-                (getContext(), "Buy tickets pressed", Toast.LENGTH_SHORT).show());
-        buttonPlayVideo.setOnClickListener(item ->
-                Toast.makeText(getContext(), "Play video is pressed", Toast.LENGTH_SHORT).show());
-        setVisibleItems(false);
-        return view;
-    }
+        viewModel = new CurrentFilmViewModel();
+        viewDataBinding = CurrentFilmFragmentBinding.bind(view);
+        viewDataBinding.setViewmodel(viewModel);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        loadFilm();
-    }
+        setupToolbar();
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
+        return viewDataBinding.getRoot();
     }
 
     @Override
@@ -139,104 +58,40 @@ public class CurrentFilmFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
-    public interface FilmLoaded {
-        void isLoaded(CurrentMovieRequest currentMovieRequest);
-    }
-
-    public interface DownloadImage {
-        void isDownloaded(CurrentMovieRequest currentMovieRequest);
-    }
-
-    public void loadFilm() {
-        DownloadImage imageDownloadCallback = (film) -> {
-            Bitmap imageFilm = film.getBackdropBitmap();
-            posterFilm.setImageBitmap(imageFilm);
-            linearLayout.setBackground(createShadow(imageFilm));
-            progressLoaderImage.setVisibility(View.INVISIBLE);
-        };
-
-        FilmLoaded filmLoadCallback = (currentMovieRequest) -> {
-            collapsingToolbarLayout.setTitle(currentMovieRequest.getTitle());
-            releaseDate.setText(currentMovieRequest.getReleaseDate());
-            voteAverage.setText(String.valueOf(currentMovieRequest.getVoteAverage()));
-            description.setText(currentMovieRequest.getOverview());
-            setVisibleItems(true);
-            String url;
-            if (currentMovieRequest.getBackdropPath() == null) {
-                url = URL_IMAGE_500PX + currentMovieRequest.getPosterPath();
-            } else {
-                url = URL_IMAGE_500PX + currentMovieRequest.getBackdropPath();
-            }
-            Target target = new Target() {
-                @Override
-                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                    currentMovieRequest.setBackdropBitmap(bitmap);
-                    imageDownloadCallback.isDownloaded(currentMovieRequest);
-                }
-
-                @Override
-                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-                }
-
-                @Override
-                public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-                }
-            };
-
-            Picasso.get().load(url).into(target);
-        };
-
-        FindFilmApi findFilmApi = RetrofitSingleton.getFindFilmApi();
-        findFilmApi.getCurrentMovie(idFilm, API_KEY, LANGUAGE)
-                .enqueue(new Callback<CurrentMovieRequest>() {
-                    @Override
-                    public void onResponse(Call<CurrentMovieRequest> call, Response<CurrentMovieRequest> response) {
-                        filmLoadCallback.isLoaded(response.body());
-                    }
-
-                    @Override
-                    public void onFailure(Call<CurrentMovieRequest> call, Throwable t) {
-
-                    }
-                });
-    }
-
-    private void setVisibleItems(boolean visible) {
-        int visibleItem;
-        if (visible) {
-            visibleItem = View.VISIBLE;
-        } else {
-            visibleItem = View.INVISIBLE;
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getArguments() != null) {
+            viewModel.start(getArguments().getInt(ARGUMENT_FILM_ID));
         }
-        posterFilm.setVisibility(visibleItem);
-        description.setVisibility(visibleItem);
-        voteAverage.setVisibility(visibleItem);
-        releaseDate.setVisibility(visibleItem);
-        buttonBuyTickets.setVisibility(visibleItem);
     }
 
-    private Drawable createShadow(Bitmap bitmap) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Palette palette = Palette.from(bitmap).generate();
-            palette.getLightMutedSwatch();
-            int color;
-            Palette.Swatch swatch = palette.getLightMutedSwatch();
-            if (swatch!=null){
-                color = swatch.getRgb();
-            } else {
-                color = palette.getDominantSwatch().getRgb();
-            }
 
-            int radiusValue = 2;
-            int[] colors = {color, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE};
-            GradientDrawable shadow = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
-            shadow.setCornerRadius(radiusValue);
-            shadow.setAlpha(150);
-            return shadow;
-        } else {
-            return null;
+
+
+    private void setupToolbar(){
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        Toolbar toolbar = (Toolbar) viewDataBinding.getRoot().findViewById(R.id.current_film_toolbar);
+        setHasOptionsMenu(true);
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+            toolbar.setNavigationOnClickListener(item -> activity.onBackPressed());
+            Objects.requireNonNull(activity.getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    public void setViewModel(CurrentFilmViewModel taskViewModel) {
+        viewModel = taskViewModel;
+    }
+
+    @BindingAdapter("android:src")
+    public static void setPosterFilm(ImageView imageView, Bitmap bitmap){
+        imageView.setImageBitmap(bitmap);
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/sasfmlzr/findfilm/fragment/currentfilm/CurrentFilmViewModel.java
+++ app/src/main/java/com/sasfmlzr/findfilm/fragment/currentfilm/CurrentFilmViewModel.java
@@ -1,4 +1,189 @@
 package com.sasfmlzr.findfilm.fragment.currentfilm;
 
-public class CurrentFilmViewModel {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.databinding.ObservableBoolean;
+import android.databinding.ObservableField;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.load.engine.Resource;
+import com.sasfmlzr.findfilm.model.RetrofitSingleton;
+import com.sasfmlzr.findfilm.request.CurrentMovieRequest;
+import com.sasfmlzr.findfilm.request.FindFilmApi;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import com.android.databinding.library.baseAdapters.BR;
+
+import static com.sasfmlzr.findfilm.model.SystemSettings.API_KEY;
+import static com.sasfmlzr.findfilm.model.SystemSettings.LANGUAGE;
+import static com.sasfmlzr.findfilm.model.SystemSettings.URL_IMAGE_500PX;
+
+public class CurrentFilmViewModel extends BaseObservable {
+
+
+    public ObservableField<Bitmap> posterFilm = new ObservableField<>();
+    public ObservableField<Bitmap> shadow = new ObservableField<>();
+    public ObservableField<String> description = new ObservableField<>();
+    public  ObservableField<String> voteAverage = new ObservableField<>();
+    public  ObservableField<String> releaseDate = new ObservableField<>();
+    public  ObservableField<String> title = new ObservableField<>();
+    public final ObservableBoolean dataLoading = new ObservableBoolean(false);
+
+
+    private String materialMessage = "Buy tickets pressed";
+    private String videoMessage = "Play video pressed";
+
+
+    @Bindable
+    public String toastMessage = null;
+
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+
+    private void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public void emptyButtonClicked(){
+        setToastMessage(videoMessage);
+    }
+
+    public ObservableField<String> getTitle() {
+        return title;
+    }
+
+    public void setTitle(ObservableField<String> title) {
+        this.title = title;
+    }
+
+    private int idFilm;
+
+    public interface FilmLoaded {
+        void isLoaded(CurrentMovieRequest currentMovieRequest);
+    }
+
+    public interface DownloadImage {
+        void isDownloaded(CurrentMovieRequest currentMovieRequest);
+    }
+
+
+    public void start(int filmId) {
+        this.idFilm = filmId;
+        loadFilm();
+    }
+
+    public void loadFilm() {
+        DownloadImage imageDownloadCallback = (film) -> {
+            Bitmap imageFilm = film.getBackdropBitmap();
+
+            posterFilm.set(imageFilm);
+
+         //   shadow.set(createShadow(imageFilm));
+            dataLoading.set(true);
+        };
+
+        FilmLoaded filmLoadCallback = (currentMovieRequest) -> {
+            title.set(currentMovieRequest.getTitle());
+            releaseDate.set(currentMovieRequest.getReleaseDate());
+            voteAverage.set(String.valueOf(currentMovieRequest.getVoteAverage()));
+            description.set(currentMovieRequest.getOverview());
+           //setVisibleItems(true);
+            String url;
+            if (currentMovieRequest.getBackdropPath() == null) {
+                url = URL_IMAGE_500PX + currentMovieRequest.getPosterPath();
+            } else {
+                url = URL_IMAGE_500PX + currentMovieRequest.getBackdropPath();
+            }
+            Target target = new Target() {
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    currentMovieRequest.setBackdropBitmap(bitmap);
+                    imageDownloadCallback.isDownloaded(currentMovieRequest);
+                }
+
+                @Override
+                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                }
+            };
+
+            Picasso.get().load(url).into(target);
+        };
+
+        FindFilmApi findFilmApi = RetrofitSingleton.getFindFilmApi();
+        findFilmApi.getCurrentMovie(idFilm, API_KEY, LANGUAGE)
+                .enqueue(new Callback<CurrentMovieRequest>() {
+                    @Override
+                    public void onResponse(Call<CurrentMovieRequest> call, Response<CurrentMovieRequest> response) {
+                        filmLoadCallback.isLoaded(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<CurrentMovieRequest> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    public void buttonBuyTicketsPressed() {
+
+    }
+
+    public void buttonPlayVideoPressed() {
+
+    }
+
+    private Drawable createShadow(Bitmap bitmap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Palette palette = Palette.from(bitmap).generate();
+            palette.getLightMutedSwatch();
+            int color;
+            Palette.Swatch swatch = palette.getLightMutedSwatch();
+            if (swatch != null) {
+                color = swatch.getRgb();
+            } else {
+                color = palette.getDominantSwatch().getRgb();
+            }
+
+            int radiusValue = 2;
+            int[] colors = {color, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE};
+            GradientDrawable shadow = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
+            shadow.setCornerRadius(radiusValue);
+            shadow.setAlpha(150);
+            return shadow;
+        } else {
+            return null;
+        }
+    }
 }
